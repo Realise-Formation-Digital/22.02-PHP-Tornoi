52a0213225507793eb3a21436b89826bf861893d
Sam <sami_kherbache@hotmail.com>
2022-04-08 11:36:34 +0200

    First Commit

---
+++ www/models/EquipeModel.php
@@ -0,0 +1,73 @@
+<?php 
+    require_once __DIR__ . "/Database.php";
+
+    class EquipeModel extends Database {
+
+        public $id;
+        public $nom;
+        public $entraineur;
+        public $logo;
+
+        public function getAllEquipe($offset = 0, $limit = 10) {
+            // ---- Montre tous les équipes par nom et maximum 10 ----
+            return $this->getMany(
+                "SELECT * FROM equipe ORDER BY nom ASC LIMIT $offset, $limit",
+                "EquipeModel"
+            );
+        }
+
+        public function getSingleEquipe($id) {
+            // ---- Montre une seule equipe par son id ----
+            return $this->getSingle(
+                "SELECT * FROM equipe WHERE id = $id",
+                "EquipeModel"
+            );
+        }
+
+        /**
+         * ---- Inserer une equipe ----
+         */
+        public function insertEquipe($array) {
+            // ---- TODO : Donne une forme a l'array ----
+            $keys = implode(", ", array_keys($array));
+            $values = implode("', '", array_values($array));
+
+            // ---- TODO : Insere une nouvelle ligne avec le key/values donne dans l'array  ----
+            return $this->insert(
+                "INSERT INTO equipe ($keys) VALUES ('$values')",
+                "EquipeModel",
+                "SELECT * FROM equipe"
+            );
+        }
+
+        /**
+         * ---- TODO : Modifier une equipe, declare son id et une array (valeur des colonne a modifie) ----
+         */
+        public function updateEquipe($array, $id) {
+            // ---- TODO : Declare un array, pour chaque cle dans l'array il prend ça valeur puis il les separe par ","  ----
+            $values_array = [];
+            foreach($array as $key => $value) {
+                $values_array[] = "$key = '$value'";
+            }
+            $values = implode(",", array_values($values_array));
+
+            // ---- TODO : Modifie une equipe selectionner par son id ----
+            return $this->update(
+                "UPDATE equipe SET $values WHERE id = $id",
+                "EquipeModel",
+                "SELECT id FROM equipe WHERE id=$id",
+                "SELECT * FROM equipe WHERE id=$id"
+            );
+        }
+
+        /**
+         * ---- TODO : Elimine une equipe par son id ----
+         */
+        public function deleteEquipe($id) {
+            // ---- Elimine une equipe par son id ----
+            return $this->delete(
+                "DELETE FROM equipe WHERE id=$id",
+                "SELECT id FROM equipe WHERE id=$id"
+            );
+        }
+    }
--- www/routes.php
+++ www/routes.php
@@ -2,13 +2,25 @@
 
 // ---- TODO : Commenter ce bout de code, qu'est-ce qu'il recherche ? ----
 require_once __DIR__ . "/controllers/BaseController.php";
-require_once __DIR__ . "/controllers/UserController.php";
+require_once __DIR__ . "/controllers/joueurController.php";
 
-// ---- TODO : Commenter ce bout de code ----
+// ---- TODO : changer le path /api/users/... ----
 $routes = [
-  "/api/users/list" => ['GET', 'UserController', 'getList'],
-  "/api/users/get" => ['GET', 'UserController', 'get'],
-  "/api/users/add" => ['POST', 'UserController', 'store'],
-  "/api/users/update" => ['PUT', 'UserController', 'update'],
-  "/api/users/remove" => ['DELETE', 'UserController', 'destroy'],
+  "/api/equipe/list" => ['GET', 'EquipeController', 'getList'],
+  "/api/equipe/get" => ['GET', 'EquipeController', 'get'],
+  "/api/equipe/add" => ['POST', 'EquipeController', 'store'],
+  "/api/equipe/update" => ['PUT', 'EquipeController', 'update'],
+  "/api/equipe/remove" => ['DELETE', 'EquipeController', 'destroy'],
+
+  "/api/joueur/list" => ['GET', 'joueurController', 'getList'],
+  "/api/joueur/get" => ['GET', 'joueurController', 'get'],
+  "/api/joueur/add" => ['POST', 'joueurController', 'store'],
+  "/api/joueur/update" => ['PUT', 'joueurController', 'update'],
+  "/api/joueur/remove" => ['DELETE', 'joueurController', 'destroy'],
+
+  "/api/tournoi/list" => ['GET', 'TournoiController', 'getList'],
+  "/api/tournoi/get" => ['GET', 'TournoiController', 'get'],
+  "/api/tournoi/add" => ['POST', 'TournoiController', 'store'],
+  "/api/tournoi/update" => ['PUT', 'TournoiController', 'update'],
+  "/api/tournoi/remove" => ['DELETE', 'TournoiController', 'destroy'],
 ];
